798f5a686a2f24b6c8bf0f81afef561a356ba971
joyqi <magike.net@gmail.com>
2021-08-14 19:49:01 +0800

    fix args

--- make-workflows.sh
+++ make-workflows.sh
@@ -47,10 +47,7 @@ do
           platforms: \${{ steps.generate.outputs.PLATFORM }}
           push: true
           tags: \${{ steps.generate.outputs.VERSION }}
-          build-args:
-            - TAG=\${{ steps.generate.outputs.TAG }}
-            - URL=\${{ steps.generate.outputs.URL }}
-            - VERSION=\${{ steps.generate.outputs.VERSION }}
+          build-args: TAG=\${{ steps.generate.outputs.TAG }},URL=\${{ steps.generate.outputs.URL }},VERSION=\${{ steps.generate.outputs.VERSION }}
 EOF
     done
 done
